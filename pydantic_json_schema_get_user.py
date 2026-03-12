7f143cd021f195c00a2bbc5d0a298e6d6068b51a
seegrendel <seegrendel@gmail.com>
2026-03-13 01:56:35 +0300

    7.3 Валидация JSON schema

---
+++ pydantic_json_schema_get_user.py
@@ -0,0 +1,31 @@
+from clients.private_http_builder import AuthenticationUserSchema
+from clients.users.public_users_client import get_public_users_client
+from clients.users.private_users_client import get_private_users_client
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
+from tools.assertions.schema import validate_json_schema
+from tools.fakers import get_random_email
+
+public_users_client = get_public_users_client()
+
+create_user_request = CreateUserRequestSchema(
+    email=get_random_email(),
+    password="string",
+    last_name="string",
+    first_name="string",
+    middle_name="string"
+)
+create_user_response = public_users_client.create_user_api(create_user_request)
+
+authentication_user = AuthenticationUserSchema(
+    email=create_user_request.email,
+    password=create_user_request.password
+)
+
+private_users_client = get_private_users_client(authentication_user)
+
+user_id = create_user_response.json().get("user").get("id")
+
+get_user_response = private_users_client.get_user(user_id)
+get_user_response_schema = GetUserResponseSchema.model_json_schema()
+
+validate_json_schema(instance=get_user_response.model_dump(by_alias=True), schema=get_user_response_schema)
